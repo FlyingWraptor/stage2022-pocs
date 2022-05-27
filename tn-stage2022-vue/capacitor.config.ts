fe896f38bb3ba563307f3fe48a17c347ba31cd55
FlyingWraptor <thibaut.nijs@groupclaes.be>
2022-05-27 12:26:23 +0200

    Add base vue ionic project

---
+++ tn-stage2022-vue/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tn-stage2022-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
